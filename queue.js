cde3c4070507fbcc0fca96f93cfdd26f8a695dd2
Maor Israel <maorisr@gmail.com>
2021-08-05 19:43:23 +0300

    Added more codes

---
+++ queue.js
@@ -0,0 +1,85 @@
+// Queue class
+class Queue {
+    // Array is used to implement a Queue
+    constructor() {
+        this.items = [];
+    }
+
+    // enqueue function
+    enqueue(element) {
+        // adding element to the queue
+        this.items.push(element);
+    }
+
+    // dequeue function
+    dequeue() {
+        // removing element from the queue
+        // returns underflow when called
+        // on empty queue
+        if (this.isEmpty())
+            return "Underflow";
+        return this.items.shift();
+    }
+
+    // front function
+    // returns the Front element of
+    // the queue without removing it.
+    front() {
+        if (this.isEmpty())
+            return "No elements in Queue";
+        return this.items[0];
+    }
+
+// isEmpty function
+    isEmpty() {
+        // return true if the queue is empty.
+        return this.items.length === 0;
+    }
+
+// printQueue function
+    printQueue() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++)
+            str += this.items[i] + " ";
+        return str;
+    }
+}
+
+// Driver Code
+// creating object for queue class
+const queue = new Queue();
+
+
+// Testing dequeue and pop on an empty queue
+// returns Underflow
+console.log(queue.dequeue());
+
+// returns true
+console.log(queue.isEmpty());
+
+// Adding elements to the queue
+// queue contains [10, 20, 30, 40, 50]
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+queue.enqueue(60);
+
+// returns 10
+console.log(queue.front());
+
+// removes 10 from the queue
+// queue contains [20, 30, 40, 50, 60]
+console.log(queue.dequeue());
+
+// returns 20
+console.log(queue.front());
+
+// removes 20
+// queue contains [30, 40, 50, 60]
+console.log(queue.dequeue());
+
+// printing the elements of the queue
+// prints [30, 40, 50, 60]
+console.log(queue.printQueue());
